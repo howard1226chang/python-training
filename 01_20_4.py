59f1c4b89c65d13291445bba66f18744df069c52
howard1226chang <136425563+howard1226chang@users.noreply.github.com>
2026-01-20 07:08:12 +0800

    Add files via upload

---
+++ 01_20_4.py
@@ -0,0 +1,12 @@
+num_lt = []
+max_lt = [0,0,0]
+for i in range(10):
+    num = int(input())
+    num_lt.append(num)
+    for o in range(3):
+        if(max_lt[o]<num):
+            temp = max_lt[o]
+            max_lt[o] = num
+            num = temp
+            print(max_lt)
+#print(max_lt)
